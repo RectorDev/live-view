85ebf49e100f6c088e1ad64c8becdb03a1ee04f3
RectorDev <Rector.dev@gmail.com>
2024-12-26 12:03:02 +0330

    initial commit

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ let tag = document.createElement("script");
 tag.src = "https://www.youtube.com/player_api";
 let firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// Replace the 'ytplayer' element with an <iframe> and
-
-// YouTube player after the API code downloads.
-
 let player;
 function onYouTubePlayerAPIReady() {
   player = new YT.Player("ytplayer", {
     height: "180",
     width: "180",
     events: {
-    onStateChange: playerStateHandle,
+      onStateChange: playerStateHandle,
       onReady: startPanel, //Firing Timer Function
     },
     playerVars: {
@@ -30,17 +25,14 @@ function onYouTubePlayerAPIReady() {
     },
   });
 }
-
 // starts panel functionality when video is Ready
 function startPanel() {
   startTimer();
 }
 // Fire when player state changes
-function playerStateHandle(e){
-    setTitle()
-    console.log(e);
-    
-
+function playerStateHandle(e) {
+  setTitle();
+  console.log(e);
 }
 
 // title
@@ -95,22 +87,68 @@ speaker.addEventListener("click", () => {
 // Video Control
 const nextVideo = document.querySelector("#next");
 const previousVideo = document.querySelector("#previous");
-nextVideo.addEventListener("click", ()=>{
-    player.nextVideo();
-    
+nextVideo.addEventListener("click", () => {
+  player.nextVideo();
 });
-previousVideo.addEventListener("click",()=>{
-    player.previousVideo();
-} );
+previousVideo.addEventListener("click", () => {
+  player.previousVideo();
+});
 
 // Radio List
-
+// -- variables
 const radioList = [
-  { title: "", Channel: "", Key: "" },
-  { title: "", Channel: "", Key: "" },
-  { title: "", Channel: "", Key: "" },
-  { title: "", Channel: "", Key: "" },
-  { title: "", Channel: "", Key: "" },
-  { title: "", Channel: "", Key: "" },
-  { title: "", Channel: "", Key: "" },
+  {
+    title: "📻 Lofi Girl - Radios",
+    Channel: "LIVE Lofi Girl",
+    Key: "PL6NdkXsPL07Il2hEQGcLI4dg_LTg7xA2L",
+  },
+  {
+    title: "Play all Play all HELIOS DEEP • 24/7 Live Radio",
+    Channel: "Helios Deep",
+    Key: "PL1eEkhVDbtH_2x8HRBmcbAhVNJnNgiEKB",
+  },
+  {
+    title: "24/7 Radio Playlist ",
+    Channel: "Rap Mafia",
+    Key: "PLWFs1ZghJen_eHkbRROt7F3HxndADTxSA",
+  },
+  {
+    title: "1 Hour of Lofi Hip Hop",
+    Channel: "the bootleg boy 2",
+    Key: "PLF3eNE6vR-4WsBf8qnJLqBywqX39QczxJ",
+  },
+  {
+    title: "The Best of Chill Music Lab",
+    Channel: "Chill Music Lab",
+    Key: "PLdE7uo_7KBkf6X1lbOpL3tAWERvlYej2L",
+  },
+  {
+    title: "TRAP SOUL 2023",
+    Channel: "Tower Beatz",
+    Key: "PLMYCrD1bUbjU9XW8BVk-SbdNPrNsT1RaT",
+  },
+  {
+    title: "Lofi Study Music",
+    Channel: "MixHound",
+    Key: "PLeiII6jxXbCD41vuxz0i6q5j9zDbMK6B6",
+  },
 ];
+
+// -- functionality
+// Function to load playlist
+function loadPlaylist(key) {
+  player.loadPlaylist({ list: key });
+  console.log(`Loading playlist: ${key}`);
+}
+const radios = document.querySelector(".radio-list");
+radios.innerHTML = radioList
+  .map((element) => {
+    return `<div class="radio-item">
+      <h3>${element.title}</h3>
+      <p>Channel: ${element.Channel}</p>  
+      <button onclick="loadPlaylist('${element.Key}')">Play Playlist</button>
+    </div>`;
+  })
+  .join("");
+
+// tests
